768b0b3a997566d648cfd8d4ba7707232061153b
rmcclorey1125 <rmcclorey1@yahoo.com>
2020-07-27 18:07:07 -0400

    Log in

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,14 @@
 class FavoritesController < ApplicationController
+
+    def create 
+        favorite =  Favorite.where(:user_id => favorite_params[:user_id], :anime_id => favorite_params[:anime_id]).first_or_create
+        render json: favorite, include: [:user, :anime]
+      end 
+    
+    
+      private 
+      def favorite_params
+        params.require(:favorite).permit! 
+      end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
 
+    def login
+        user = User.find_by(user_params)
+        render json: user, include: [:animes] 
+    end
+
+    def create
+        user = User.create(user_params)
+        render json: user, include: [:animes]
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit! 
+    end
     
 end
